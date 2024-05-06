6c1712e4caa61f6c2cab57f60fb4fa129e618ff8
NishuRajput12 <161692122+NishuRajput12@users.noreply.github.com>
2024-05-06 15:47:04 +0530

    Add files via upload

---
+++ activity todo.js
@@ -0,0 +1,39 @@
+let btn=document.querySelector("button");
+let ul=document.querySelector("ul");
+let inp=document.querySelector("input");
+//btn.addEventListener("click",function(){
+  // console.log(inp.value);
+  //inp.value="";
+//});
+btn.addEventListener("click",function(){
+    let item=document.createElement("li");
+    item.innerText=inp.value;
+    let delBtn=document.createElement("button");
+    delBtn.innerText="delete";
+    delBtn.classList.add("delete");
+    item.appendChild(delBtn);
+    ul.appendChild(item);
+ 
+    inp.value="";
+
+});
+ul.addEventListener("click",function(event){
+    //console.log(event.target);
+    console.dir(event.target.nodeName=="BUTTON");{
+        let listItem=event.target.parentElement;
+        listItem.remove();
+    
+    console.log("button clicked");
+    }
+});
+//let delBtns=document.querySelectorAll(".delete")
+//for( delBtn of delBtns){
+    //delBtn.addEventListener("click",function(){
+//let par=this.parentElement;
+//console.log(par);
+    
+
+//par.remove();
+
+//});
+//}
